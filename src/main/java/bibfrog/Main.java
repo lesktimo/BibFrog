8522e188bd29f0e08e8e0ead15a2f3d88a208f44
lesktimo <l3skt1m@gmail.com>
2017-04-04 17:23:40 +0300

    deleted gradle files

--- src/main/java/bibfrog/Main.java
+++ src/main/java/bibfrog/Main.java
@@ -1,8 +1,5 @@
 package bibfrog;
 
-
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/bibfrog/controller/InproceedingsController.java
+++ src/main/java/bibfrog/controller/InproceedingsController.java
@@ -1,9 +1,18 @@
-
 package bibfrog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(value = "/inpro")
 public class InproceedingsController {
-    
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String addInproceeding(Model model) {
+        return "inpro";
+    }
+
 }
