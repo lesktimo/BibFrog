461ceccc52444ba30964cb1f4b59c3c370658c58
juslesan <santeri.juslenius@gmail.com>
2017-04-11 16:19:57 +0300

    bittimuotoon

--- src/test/java/bibfrog/StepDefs.java
+++ src/test/java/bibfrog/StepDefs.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class StepDefs {
 
-    String baseUrl = "http://localhost:8080";
+    String baseUrl = "http://127.0.0.1:8080";
 
     WebDriver driver = new HtmlUnitDriver();
 
