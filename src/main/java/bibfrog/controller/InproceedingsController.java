ceaf1f89fcb59a18903d07232b9988dba50d8968
Toivonen Kim P <tokito@dat-13.cs.helsinki.fi>
2017-04-19 14:51:25 +0300

    Download function in inproceeding controller created and started

--- src/main/java/bibfrog/controller/InproceedingsController.java
+++ src/main/java/bibfrog/controller/InproceedingsController.java
@@ -3,12 +3,15 @@ package bibfrog.controller;
 import bibfrog.domain.Inproceeding;
 import bibfrog.repositories.InproceedingsRepo;
 import bibfrog.service.ExportService;
+import java.io.File;
+import java.io.IOException;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -42,6 +45,19 @@ public class InproceedingsController {
     public String listInproceedings(Model model) {
         model.addAttribute("inproList", inproRepo.findAll());
         return "inpros";
+    }
+    
+    @RequestMapping(value ="/inpro/{id}/download", method = RequestMethod.GET)
+    public void downloadInpro(@PathVariable Long id) throws IOException {
+        Inproceeding inpro = inproRepo.findOne(id);
+        String bibtex = exportService.createBibtexFromInproceeding(inpro);
+        exportService.createFile(bibtex);
+        
+        File inproFile = new File("filepath tähän");
+        
+        
+        
+        
     }
 
 }
