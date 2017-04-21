9377ea1d032e37ae2834c90333198fa490d9affe
Matias Isosaari <matias_isosaari@hotmail.com>
2017-04-21 16:37:19 +0300

    InproceedingTest

--- src/test/java/bibfrog_JUnit/InproceedingTest.java
+++ src/test/java/bibfrog_JUnit/InproceedingTest.java
@@ -45,4 +45,31 @@ public class InproceedingTest {
         inpro.setAuthors();
         assertEquals("Kermit The Frog", inpro.authorString());
     }
+    
+    @Test
+    public void optionalFieldsReturnsOptionalValuesInCorrectOrder() {
+        inpro.setEditor("test1");;
+        inpro.setVolume(1);;
+        inpro.setSeries("test2");
+        inpro.setPages("test3");
+        inpro.setAddress("test4");
+        inpro.setPublishMonth(12);
+        inpro.setOrganization("test5");
+        inpro.setPublisher("test6");
+        inpro.setNote("test7");
+        
+        Inproceeding expected = new Inproceeding();
+        
+        expected.setEditor("test1");;
+        expected.setVolume(1);;
+        expected.setSeries("test2");
+        expected.setPages("test3");
+        expected.setAddress("test4");
+        expected.setPublishMonth(12);
+        expected.setOrganization("test5");
+        expected.setPublisher("test6");
+        expected.setNote("test7");
+        
+        assertEquals(expected.optionalFields(), inpro.optionalFields());
+    }
 }
