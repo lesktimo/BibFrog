27d1ca07d024ab5d92d8726fae541c926e99f867
Matias Isosaari <matias_isosaari@hotmail.com>
2017-05-03 14:22:48 +0300

    doccii

--- src/main/java/bibfrog/service/FileService.java
+++ src/main/java/bibfrog/service/FileService.java
@@ -7,18 +7,39 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service for returning specified file, creating headers and filepaths.
+ */
 @Service
 public class FileService {
 
+    /**
+     * Return file with specified filepath.
+     *
+     * @param filePath
+     * @return specified file
+     */
     public File getFilePathForBytes(String filePath) {
         return new File(filePath);
-
     }
 
+    /**
+     * Creates path for given file.
+     *
+     * @param file
+     * @return filepath
+     */
     public Path createPath(File file) {
         return Paths.get(file.getPath());
     }
 
+    /**
+     * Creates HttpHeaders for given file.
+     *
+     * @param file
+     * @param fileName
+     * @return HttpHeaders
+     */
     public HttpHeaders createHeaders(File file, String fileName) {
         final HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.TEXT_PLAIN);
