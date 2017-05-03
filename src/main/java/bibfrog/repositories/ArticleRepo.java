56ec8f7531d32568a7a7213e7612d9cf89b90432
Ollipekka Väänänen <Ollipekka.vaananen@helsinki.fi>
2017-05-03 15:14:37 +0300

    repot

--- src/main/java/bibfrog/repositories/ArticleRepo.java
+++ src/main/java/bibfrog/repositories/ArticleRepo.java
@@ -1,10 +1,16 @@
 package bibfrog.repositories;
 
 import bibfrog.domain.Article;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepo extends JpaRepository<Article, Long> {
+
+List<Article> findByAuthors(String author);
+List<Article> findByPublisher(String publisher);
+List<Article> findByTitle(String title);
+List<Article> findByPublishYear(int year);
 
 }
--- src/main/java/bibfrog/repositories/InproceedingsRepo.java
+++ src/main/java/bibfrog/repositories/InproceedingsRepo.java
@@ -1,10 +1,18 @@
 package bibfrog.repositories;
 
 import bibfrog.domain.Inproceeding;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InproceedingsRepo extends JpaRepository<Inproceeding, Long> {
+
+List<Inproceeding> findByAuthors(String author);    
+List<Inproceeding> findByBookTitle(String booktitle);
+List<Inproceeding> findByTitle(String title);
+List<Inproceeding> findByPublishYear(int year);
+
+
 
 }
